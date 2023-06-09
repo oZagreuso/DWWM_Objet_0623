483814992c991a2a6030f3e28adfd4afd93b4420
Olivier Dal Ferro <120396212+oZagreuso@users.noreply.github.com>
2023-06-09 08:41:30 +0200

    UpdateAM08h41_090623

--- CL_Bouteille/Bouteille.cs
+++ CL_Bouteille/Bouteille.cs
@@ -14,55 +14,56 @@ namespace CL_Bouteille
         private string nomBouteille;
         private string matiereBouteille;
         private double contenanceBouteilleEnLitres;
-        private double contenuNiveauBouteilleEnLitres;
+        private double contenuBouteilleEnLitres;
         private string typeNomContenuBouteille;
+        private double quantiteEnLitres;
         private bool estFermee;
         //private bool estVide;
 
         //Constructeurs (avec trois constructeurs, polymorphisme de constructeur
 
-            //Par défaut
+        //Par défaut
 
         public Bouteille()
         {
             nomBouteille = " Pepsi Max ";
             matiereBouteille = " Plastique ";
             contenanceBouteilleEnLitres = 1.25;
-            contenuNiveauBouteilleEnLitres = 0.75;
+            contenuBouteilleEnLitres = 0.75;
             typeNomContenuBouteille = " soda ";
+            quantiteEnLitres = 0.25;
             estFermee = true;
             //estVide = false;
         }
 
-            //Classique
+        //Classique
 
-        public Bouteille(string _nomBouteille, string _matiereBouteille, double _contenanceBouteilleEnLitres, double _contenuNiveauBouteilleEnLitres, string _typeNomContenuBouteille, bool _estFermee, bool _estVide)
+        public Bouteille(string _nomBouteille, string _matiereBouteille, double _contenanceBouteilleEnLitres, double _contenuBouteilleEnLitres, string _typeNomContenuBouteille, double _quantiteEnLitres, bool _estFermee, bool _estVide)
         {
             this.nomBouteille = _nomBouteille;
             this.matiereBouteille = _matiereBouteille;
             this.contenanceBouteilleEnLitres = _contenanceBouteilleEnLitres;
-            this.contenuNiveauBouteilleEnLitres = _contenuNiveauBouteilleEnLitres;
+            this.contenuBouteilleEnLitres = _contenuBouteilleEnLitres;
             this.typeNomContenuBouteille = _typeNomContenuBouteille;
-            this.estFermee= _estFermee;
+            this.quantiteEnLitres = _quantiteEnLitres;
+            this.estFermee = _estFermee;
             //this.estVide= _estVide;
         }
 
-            //Par clonage
+        //Par clonage
 
         public Bouteille(Bouteille cloneBouteille)
         {
             this.nomBouteille = cloneBouteille.nomBouteille;
             this.matiereBouteille = cloneBouteille.matiereBouteille;
             this.contenanceBouteilleEnLitres = cloneBouteille.contenanceBouteilleEnLitres;
-            this.contenuNiveauBouteilleEnLitres = cloneBouteille.contenuNiveauBouteilleEnLitres;
+            this.contenuBouteilleEnLitres = cloneBouteille.contenuBouteilleEnLitres;
             this.typeNomContenuBouteille = cloneBouteille.typeNomContenuBouteille;
+            this.quantiteEnLitres = cloneBouteille.quantiteEnLitres;
             this.estFermee = cloneBouteille.estFermee;
-            //this.estVide = cloneBouteille.estVide;
         }
 
-
         //Methodes
-
         public string GetNomBouteille()
         {
             return this.nomBouteille;
@@ -79,24 +80,64 @@ namespace CL_Bouteille
         }
         private double GetContenuBouteilleEnLitres()
         {
-            return 
+            return this.contenuBouteilleEnLitres;
         }
-        public bool FermerBouteille;
-        public bool OuvrirBouteille;
-
-        //remplirTout
-        //viderTout
-        public bool RemplirBouteille(double remplirQuantiteEnLitres)
+        public bool FermerBouteille()
         {
             if (this.estFermee)
-            {                
+            {
                 return false;
             }
             else
             {
-                if (this.contenuNiveauBouteilleEnLitres < this.contenanceBouteilleEnLitres)
+                return true;
+            }
+        }
+        public bool OuvrirBouteille()
+        {
+            if (this.estFermee)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool RemplirTout()
+        {
+            if (this.estFermee = false && this.contenuBouteilleEnLitres < this.contenanceBouteilleEnLitres)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool ViderTout()
+        {
+            if (this.estFermee = false && this.contenuBouteilleEnLitres > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool RemplirBouteille(double quantiteEnLitres)
+        {
+            if (this.estFermee)
+            {
+                return false;
+            }
+            else
+            {
+                if (this.contenuBouteilleEnLitres < this.contenanceBouteilleEnLitres)
                 {
-                    this.contenanceBouteilleEnLitres = this.contenanceBouteilleEnLitres + this.contenuNiveauBouteilleEnLitres;
+                    this.contenuBouteilleEnLitres = this.contenuBouteilleEnLitres + this.quantiteEnLitres;
                     return true;
                 }
                 else
@@ -105,7 +146,7 @@ namespace CL_Bouteille
                 }
             }
         }
-        public bool ViderBouteille(double viderBouteilleEnLitres)
+        public bool ViderBouteille(double quantiteEnLitres)
         {
             if (this.estFermee)
             {
@@ -113,18 +154,22 @@ namespace CL_Bouteille
             }
             else
             {
-                if(this.contenuNiveauBouteilleEnLitres > 0)
+                if (this.contenuBouteilleEnLitres > 0)
                 {
-
+                    this.contenuBouteilleEnLitres = this.contenuBouteilleEnLitres - this.quantiteEnLitres;
+                    return true;
+                }
+                else
+                {
+                    return false;
                 }
             }
-            bool viderOk;
-            return true;
         }
 
     }
-
 }
-    
+
+
+
 
 
