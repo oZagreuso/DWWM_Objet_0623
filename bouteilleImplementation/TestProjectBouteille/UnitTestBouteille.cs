4693c4cb5a69a7d43484e3315193db8c8aeaf748
Olivier Dal Ferro <120396212+oZagreuso@users.noreply.github.com>
2023-06-13 16:50:47 +0200

    UpdateFinal16h51_130623

--- bouteilleImplementation/TestProjectBouteille/UnitTestBouteille.cs
+++ bouteilleImplementation/TestProjectBouteille/UnitTestBouteille.cs
@@ -16,31 +16,46 @@ namespace TestProjectBouteille
             Assert.IsTrue(bTest.ContenanceBouteilleEnLitres == 1.25);
             Assert.IsTrue(bTest.ContenuBouteilleEnLitres == 0.75);            
         }
-  
-        //meme test
 
+        //meme test
+        [TestMethod]
         public void TestMethod_Bouteille_Ouvrir_EtatFermee_ReturnFalse()
         {
             Bouteille bFermee = new Bouteille();
             bool fermeeOk = bFermee.OuvrirBouteille();
-            Assert.IsTrue(fermeeOk==true);
-            Assert.IsTrue(bFermee.EstFermee==false);            
+            Assert.IsTrue(fermeeOk==true);//si la bouteille est fermée
+            Assert.IsTrue(bFermee.EstFermee==false);//alors EstFermee est FAUX, on ne peut pas ouvrir la bouteille         
         }
+        [TestMethod]
         public void TestMethod_Bouteille_Ouvrir_EtatOuvert_ReturnTrue()
         {
             Bouteille bFermee = new Bouteille();
             bool fermeeOk = bFermee.FermerBouteille();
-            Assert.IsTrue(fermeeOk==false);
-            Assert.IsTrue(bFermee.EstFermee==true);
+            Assert.IsTrue(fermeeOk==false);//si la bouteille est ouverte
+            Assert.IsTrue(bFermee.EstFermee==true);//alors EstFermee est VRAI, on peut ouvrir la bouteille
         }
-        public void TestMethod_Bouteille_Fermer_EtatFermee_ReturnTrue()
+        [TestMethod]
+        public void TestMethod_Bouteille_Fermer_EtatFermee_ReturnFalse()
         {
             Bouteille bFermee = new Bouteille();
             bool fermeeOk = bFermee.FermerBouteille();
-            Assert.IsTrue(fermeeOk);
-            Assert.IsTrue(bFermee.EstFermee==false);
+            Assert.IsTrue(fermeeOk);//si la bouteille est fermée
+            Assert.IsTrue(bFermee.EstFermee==false);//alors EstFermee est FAUX, on ne peut pas fermer la bouteille
         }
-        public void TestMethod_Bouteille_Fermer_EtatOuvert
+        [TestMethod]
+        public void TestMethod_Bouteille_Fermer_EtatOuvert_ReturnTrue()
+        {
+            Bouteille bFermee = new Bouteille();
+            bool fermeeOk = bFermee.FermerBouteille();
+            Assert.IsTrue(!fermeeOk);//si la bouteille est ouverte
+            Assert.IsTrue(bFermee.EstFermee==true);//alors EstFermee est VRAI, on peut fermer la bouteille
+        }
+        [TestMethod]
+        public void TestMethod_Bouteille_RemplirTout_EtatFermee_ReturnFalse()
+        {
+            Bouteille bFermee = new Bouteille();
+            bool fermeeOk = bFermee.RemplirTout();
+        }
         
 
         
