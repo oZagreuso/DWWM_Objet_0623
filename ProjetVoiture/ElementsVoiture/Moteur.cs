c1d846998ccdebde8a76ffefae4acb5dbe51a66a
Olivier Dal Ferro <120396212+oZagreuso@users.noreply.github.com>
2023-06-20 16:07:30 +0200

    Update16h07_200623

---
+++ ProjetVoiture/ElementsVoiture/Moteur.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CL_Voiture
+{
+    //Attribut
+    public class Moteur
+    {
+        private bool EstOn = false;
+     
+        private bool DemarrerMoteur;
+    }
+
+
+}
