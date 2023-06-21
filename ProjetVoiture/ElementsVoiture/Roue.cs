b980b71447f72728da75e4b677427880398f6914
Olivier Dal Ferro <120396212+oZagreuso@users.noreply.github.com>
2023-06-21 08:41:29 +0200

    Update08h41_210623

--- ProjetVoiture/ElementsVoiture/Roue.cs
+++ ProjetVoiture/ElementsVoiture/Roue.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace CL_Voiture
 {
     public class Roue
--- ProjetVoiture/Voiture.cs
+++ ProjetVoiture/Voiture.cs
@@ -6,7 +6,53 @@ using System.Threading.Tasks;
 
 namespace CL_Voiture
 {
-    internal class Voiture
+    public class Voiture
     {
+        //Attribut
+        private string marqueVoiture;
+        private bool estDemarre;
+
+        public string MarqueVoiture { get => marqueVoiture; private set => marqueVoiture = value; }
+        public bool EstDemarre { get => estDemarre; set => estDemarre = value; }
+
+        //Constructeur
+        public Voiture(string _marqueVoiture, bool _estDemarre)
+        {
+            this.marqueVoiture = _marqueVoiture;
+            this.estDemarre = _estDemarre;
+        }
+        public bool Demarrer()
+        {
+            if (estDemarre)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        public bool Eteindre()
+        {
+            if (estDemarre)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool Avancer()
+        {
+            if (!estDemarre)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
     }
 }
