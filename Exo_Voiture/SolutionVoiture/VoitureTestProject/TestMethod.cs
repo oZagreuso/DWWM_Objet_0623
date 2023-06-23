57460ac26702465ad4185c3d2726914d4af697dc
Olivier Dal Ferro <120396212+oZagreuso@users.noreply.github.com>
2023-06-23 11:56:56 +0200

    UpdateAMWE_230623

--- Exo_Voiture/SolutionVoiture/VoitureTestProject/TestMethod.cs
+++ Exo_Voiture/SolutionVoiture/VoitureTestProject/TestMethod.cs
@@ -7,12 +7,18 @@ namespace VoitureTestProject
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethodVoitureInstanciation()
+        public void TestMethod_Voiture_Instanciation()
         {
             Voiture vTest = new Voiture();
             Assert.IsTrue(vTest.MarqueVoiture == " Tesla ");
             Assert.IsTrue(vTest.SesQuatresRoues == new Roue[4] { new Roue(), new Roue(), new Roue(), new Roue() });
             Assert.IsTrue(vTest.SonMoteur == new Moteur());
+        }
+        [TestMethod]
+        public void TestMethod_Moteur_DemarrerMoteur_EstOn_Return_False()
+        {
+            Voiture vTestMoteurDemarrage = new Voiture();
+            Assert.IsTrue(vTestMoteurDemarrage.Demarrer = true);
         }
     }
 }
