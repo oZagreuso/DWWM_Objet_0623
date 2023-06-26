cf6e0eb93228e777ae3b8d0e48f2b401700ed0fe
Olivier Dal Ferro <120396212+oZagreuso@users.noreply.github.com>
2023-06-26 15:48:42 +0200

    UpdatePM3h49_260623

--- Exo_CompteBancaire/SolutionCompteBancaire/CompteBancaire/CompteBancaire/CL_CompteBancaire/Class1.cs
+++ Exo_CompteBancaire/SolutionCompteBancaire/CompteBancaire/CompteBancaire/CL_CompteBancaire/Class1.cs
@@ -5,64 +5,79 @@
         //Attributs
         private string nomProprietaireCompte;
         private uint numeroCompte;
-        private double saisieMontant;
         private double soldeCompte;
         private int soldeMinimum;
-        private bool debiterOk;
+
 
         //Constructeur par défaut
 
         public CompteBancaire()
-            : this("Cagnetta", 696969, 400, 250, -300, true)
+            : this("Cagnetta", 696969, 250, -300)
         {
 
         }
 
         //Constructeur classique
 
-        public CompteBancaire(string _nomProprietaireCompte, uint _numeroCompte, double _saisieMontant, double _soldeCompte, int _soldeMinimum, bool debiterOk)
+        public CompteBancaire(string _nomProprietaireCompte, uint _numeroCompte, double _soldeCompte, int _soldeMinimum)
         {
             this.nomProprietaireCompte = _nomProprietaireCompte;
             this.numeroCompte = _numeroCompte;
-            this.saisieMontant = _saisieMontant;
             this.soldeCompte = _soldeCompte;
             this.soldeMinimum = _soldeMinimum;
-            this.debiterOk = debiterOk;
         }
 
         //Constructeur par clonage
 
         public CompteBancaire(CompteBancaire cloneCompteBancaire)
-            : this(cloneCompteBancaire.nomProprietaireCompte, cloneCompteBancaire.numeroCompte, cloneCompteBancaire.saisieMontant, cloneCompteBancaire.soldeCompte, cloneCompteBancaire.soldeMinimum, cloneCompteBancaire.debiterOk)
+            : this(cloneCompteBancaire.nomProprietaireCompte, cloneCompteBancaire.numeroCompte, cloneCompteBancaire.soldeCompte, cloneCompteBancaire.soldeMinimum)
         {
 
         }
 
         //Getter-Setter
 
-        public string NomProprietaireCompte {  get => nomProprietaireCompte; set => nomProprietaireCompte = value; }
-        public uint NumeroCompte { get => numeroCompte; set => numeroCompte = value; }
-        public double SaisieMontant { get => saisieMontant; set => saisieMontant = value; }
-        public double SoldeCompte { get => soldeCompte; set => soldeCompte = value; }
-        public int SoldeMinimum { get => soldeMinimum; set => soldeMinimum = value; }
-        
+        public string NomProprietaireCompte { get => nomProprietaireCompte; private set => nomProprietaireCompte = value; }
+        public uint NumeroCompte { get => numeroCompte; private set => numeroCompte = value; }
+        public double SoldeCompte { get => soldeCompte; private set => soldeCompte = value; }
+        public int SoldeMinimum { get => soldeMinimum; private set => soldeMinimum = value; }
+
         //Méthodes
 
-
-       
-        public bool DebiterCompte()
+        public void CrediterCompte(double saisieMontant)
         {
-            if (this.debiterOk)
+           if ()
+        }
+     
+        public bool DebiterOK(double saisieMontant)
+        {
+            bool debiterOk = true;
+            if (soldeCompte - saisieMontant < soldeMinimum)
+            {
+                debiterOk = false;
+            }
+            else if (soldeCompte - saisieMontant > soldeMinimum)
             {
                 debiterOk = true;
-                return true;
+                
+            }
+            return debiterOk;
+        }
+
+        public double DebiterCompte(double saisieMontant)
+        {
+            do (double TransfererMontant(double saisie Montant))
+                { 
+            if (DebiterOK(saisieMontant))
+            {
+                bool transfererMontant = true;
+                double nouveauSolde = soldeCompte + saisieMontant;
+                nouveauSolde = soldeCompte;               
             }
             else
             {
-                debiterOk = false;
-                return false;
+               
             }
         }
-
     }
 }
